f2225b3cef243ea50d09d13be3252ea60e1b0911
ToeiPanatda <teayti45p@gmail.com>
2025-06-13 11:58:24 +0700

    update file HelloWorld and variablles

---
+++ src/com/project/java_basic/TypeVariables.java
@@ -0,0 +1,17 @@
+package com.project.java_basic;
+
+public class TypeVariables {
+    public static void main(String[] args) {
+        String name = "Toei"; //ประกาศตัวแปรแบบข้อความ
+        System.out.println(name);
+
+        int number = 7;
+        System.out.println(number);
+
+        double num = 15.44;
+        char A ='b';
+        boolean result = false;
+
+        System.out.println(A+num+"Toei");
+    }
+}
